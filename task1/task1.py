ba793a5b0f0285a3ec651a0e8e4e9acfbd6c0de9
root <root@Esonic.localdomain>
2022-05-17 08:31:44 +0300

    Rep create

---
+++ task1/task1.py
@@ -0,0 +1,21 @@
+import sys
+
+
+n = int(sys.argv[1])
+m = int(sys.argv[2])
+
+number = 1
+line = []
+
+while '1' not in line:
+    if number + m - 1 > n:
+        number += m - n - 1
+        line.append(str(number))
+    else:
+        number += m - 1
+        line.append(str(number))
+
+
+print('1', end='')
+for index in line[:-1]:
+    print(index, end='')
